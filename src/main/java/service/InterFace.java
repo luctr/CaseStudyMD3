8c2820d7e83bd8bc1007fb1679ef40977d5984ae
luctr <dinh.luc.3192@gmail.com>
2021-07-06 10:05:42 +0700

    add file

---
+++ src/main/java/service/InterFace.java
@@ -0,0 +1,12 @@
+package service;
+
+import java.util.List;
+
+public interface InterFace<T> {
+    List<T> findAll();
+    T findById(int id);
+    List<T> findByName(String name);
+    void save(T t);
+    void edit(int id,T t);
+    void delete(int id);
+}
